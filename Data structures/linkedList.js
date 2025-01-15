652be146aab2fd097ccb1344ba77606b642967fb
olusegun <olusegundavids1@gmail.com>
2025-01-15 04:22:50 +0100

    Changes to be committed:

--- Data structures/linkedList.js
+++ Data structures/linkedList.js
@@ -32,6 +32,19 @@ class LinkedList {
         this.head = new_node;
     }
 
+    search(key){
+        let current = this.head;
+
+        while (current != null) {
+            if (current.data === key) {
+                return current.data
+            } else {   
+                current = current.next
+            }
+        }
+        return null
+    }
+
     repr(){
         let nodes = [];
         let current = this.head;
@@ -59,5 +72,8 @@ list.add(12),
 list.add(2);
 list.add(67);
 
+const search = list.search(2)
+
 console.log("Resulting linked list: " + list.repr());
 console.log("Size of the linked list: " + list.size());
+console.log("search result: " + JSON.stringify(search));
