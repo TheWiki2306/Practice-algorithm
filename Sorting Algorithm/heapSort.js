5203592ff764928bc52b89f474dc7d8a393ce204
olusegun <olusegundavids1@gmail.com>
2025-01-29 02:45:22 +0100

    Changes to be committed:

--- Sorting Algorithm/heapSort.js
+++ Sorting Algorithm/heapSort.js
@@ -1,13 +1,29 @@
-let array = [10, 20, 15, 30, 40];
-let num = array.length;
-let i = array.length - 1;
+// To insert in a heap (Max heap)
+function heapSortInsertion(heap, value) {
+    heap.push(value);
+    heapify_up(heap, heap.length - 1);
 
-function heapify(array, num, i) {
-    let largestElement = i;    
-    let left = (2*i);
-    let right = (2*i+1);
+    return heap;
 }
 
-function heapSort() {
-    
+function heapify_up(heap, index) {
+    let parent_index = Math.floor((index - 1) / 2 );
+
+    while (index > 0 && heap[parent_index] < heap[index]) {
+        let temp;
+        temp = heap[parent_index];
+        heap[parent_index] = heap[index];
+        heap[index] = temp;
+
+        index = parent_index;
+        parent_index = Math.floor((index - 1) / 2 );
+    }
 }
+
+let arrayList = [] 
+let result; 
+result = heapSortInsertion(arrayList, 45);
+result = heapSortInsertion(arrayList, 55);
+result = heapSortInsertion(arrayList, 3);
+result = heapSortInsertion(arrayList, 65);
+console.log(result);
