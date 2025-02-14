7008bbb7df5c26126b6fc25f0746acc37e7f038e
olusegun <olusegundavids1@gmail.com>
2025-02-14 04:57:50 +0100

    On branch master

--- Sorting Algorithm/Non-comparison sort/counting sort/countingSort.js
+++ Sorting Algorithm/Non-comparison sort/counting sort/countingSort.js
@@ -1,20 +1,39 @@
-function frequencyCount(arrayList) {
-    // const maxElement = Math.max(arrayList);
-    // const number_of_elements = arrayList.length;
-    // let count = [];
-        
-    const frequencyMap = new Map();
+// // The idea behind this algorithm is to first create and empty array. Ths array will be used to
+// // count the frequency of the elemnts in the original array. The count array is initially initialized to 0.
+// // Then the array is updated by 1, with each appearance of an element in the original array.
+// // After that, starting from the minimun element we start updating the original array with the sorted elements with respect to the number of times they appear.
+ 
 
-    for (const item of arrayList) {
-        frequencyMap.set(item, (frequencyMap.get(item) || 0) + 1);
-    }
 
-    frequencyMap.forEach((count, item) => {
-        document.writeln(`${item} ${count}<br/>`)
-    })
 
+function countingSort(array) {
+  if (array.length === 0) {
+    return [];
+  }
+
+  let maxElement = Math.max(...array);
+  let minElement = Math.min(...array);
+  let count = new Array(maxElement - minElement + 1).fill(0);
+
+  for (let i = 0; i < array.length; i++) {
+    count[array[i] - minElement]++;
+
+  }
+
+  let sortedArray = [];
+  for (let i = 0; i < count.length; i++) {
+    const element = i + minElement
+    while (count[i] >  0) {
+      sortedArray.push(element);
+      count[i]--;
+    }    
+  }
+
+  return sortedArray;
+ 
 }
 
-const arrayList = [5, 2, 9, 7, 5, 8, 9, 5, 2, 3, 3];
-let result = frequencyCount(arrayList);
+
+const arrayList = [5, 2, 9, 7, 5, 8, 9, 5, 2, 3, 3, 4, 6, 1, 0]
+let result = countingSort(arrayList);
 console.log(result);
