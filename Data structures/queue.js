e1472a3263acf01ad06ce981132392ba9f5ca403
olusegun <olusegundavids1@gmail.com>
2025-03-04 22:28:47 +0100

    Changes to be committed:

--- Data structures/queue.js
+++ Data structures/queue.js
@@ -19,6 +19,20 @@ class Queue {
     console.log("New queue " + this.queue);
     return true;
   }
+
+  dequeue() {
+    if (this.front === -1 && this.rear === -1) {
+      console.log("Queue is empty");
+      return false;
+    } else if (this.front === this.rear) {
+      this.queue[this.front] = undefined;
+      this.front = this.rear = -1;
+    } else {
+      this.queue[this.front] = undefined;
+      this.front++;
+      console.log("New queue after dequeue: " + this.queue);
+    }
+  }
 }
 
 const queue = new Queue(5);
@@ -28,3 +42,6 @@ queue.enqueue(23);
 queue.enqueue(55);
 queue.enqueue(78);
 queue.enqueue(99);
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
