a6b13bb1830bbce232a5c87f2f71d79a54577225
olusegun <olusegundavids1@gmail.com>
2025-03-10 22:55:54 +0100

    Changes to be committed:

--- Data structures/trees/binaryTree.js
+++ Data structures/trees/binaryTree.js
@@ -1,7 +1,15 @@
 class Node {
   constructor(value) {
     this.value = value;
+    this.next = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
     this.left = null;
     this.right = null;
   }
+
+  // insert(){
 }
