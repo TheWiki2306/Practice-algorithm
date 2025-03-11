b375c9769213640f0f17c8c28dbd5fa6fe85476b
olusegun <olusegundavids1@gmail.com>
2025-03-11 02:58:45 +0100

    Changes to be committed:

--- Data structures/trees/binaryTree.js
+++ Data structures/trees/binaryTree.js
@@ -1,15 +1,45 @@
-class Node {
+class TreeNode {
   constructor(value) {
     this.value = value;
-    this.next = null;
   }
 }
 
 class BinaryTree {
   constructor() {
+    this.root = null;
     this.left = null;
     this.right = null;
   }
 
-  // insert(){
+  insert(value) {
+    const newNode = new TreeNode(value);
+
+    if (this.root == null) {
+      this.root = newNode;
+      return;
+    }
+
+    const insertNode = (node, newNode) => {
+      if (this.left) {
+        node.left = newNode;
+        console.log("left node inputed", +node.left);
+      } else {
+        insertNode(node.left, newNode);
+      }
+
+      if (this.right) {
+        node.right = newNode;
+      } else {
+        insertNode(node.right, newNode);
+        console.log("right node inputed", +node.right);
+      }
+    };
+
+    insertNode(this.root, newNode);
+  }
 }
+
+const tree = new BinaryTree(left, right);
+
+tree.insert(5);
+tree.insert();
